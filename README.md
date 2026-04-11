700d76cef1ebb54bcae13271af92bed0c0d9ff26
BALDWYN05 <207237204+BALDWYN05@users.noreply.github.com>
2026-04-11 14:59:36 -0600

    LGPIO (#1)
    * Echo Out of Range
    - added functionality so that if the echo is out of range it will send a new echo
    
    * Code Improvement
    - Switched to OOP styled coding
    - Accounts for if it loses the sound signal
    - Selectable units of measurement include cm, meters, inches, and feet
    
    * Transitioned from MTECH_GPIO to lgpio
    
    Changed from MTECH_GPIO to lgpio.
    Still need to work on multiprocessing etc
    
    Original code is used from Clinton Reimann's branch, which Mikayla Iten, and myself contributed to.
    
    This code was modified by Mikayla Iten, Nolan Wedlake, and myself.
    
    * Updated README
    
    Changed the current status of the project
    
    ---------
    
    Co-authored-by: Reimann <creimann@mtech.edu>

---
+++ README.md
@@ -0,0 +1,25 @@
+# HC-SR04 - Echo Location Sensor
+
+Branched from Clinton's Branch
+
+## Contributors:
+
+-  Dylan Baldwin
+-  Mikayla Iten
+-  Clinton Reimann
+-  Nolan Wedlake
+
+## GPIO Library
+
+- LGPIO
+
+## What does this do?
+
+- This uses a HC-SR04 Ultrasonic Sensor to read how far away the nearest object is in selectable units
+- Uses OOP for the HC-SR04
+- Comfortably reads up to 2-3 meters with decent accuracy (past that the results will be less accurate) 
+
+## What is next?
+
+- Multiprocessing
+- Servo attachment for sweeping motion <!-- Buzzer code has been written already by James and Nolan. Not including this as it will be used more for debugging purposes than for the final robot. -->
--- hc-sr04.py
+++ hc-sr04.py
@@ -1,45 +1,110 @@
-import MTECH_GPIO as GPIO
+import lgpio
 import time
+from multiprocessing import Process, Value
+import statistics
 
-# Pins and GPIO setup
-inputPin = 24
-outputPin = 23
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(inputPin, GPIO.IN) 
-GPIO.setup(outputPin, GPIO.OUT) 
+class HC_SR04:
+    def __init__(self, triggerPin:int, echoPin:int, unit = 0): #create a list of ints that can be used to label units, unit = 0 is cm
+        self.speedOfSound = 343 # this is in meters/second
+        self.echoPin = echoPin
+        self.triggerPin = triggerPin
+        self.handle =  lgpio.gpiochip_open(0)
+        lgpio.gpio_claim_output(self.handle, self.triggerPin)
+        # GPIO.setup(self.echoPin, GPIO.IN) 
+        lgpio.gpio_claim_input(self.handle, self.echoPin)
+        # GPIO.setup(self.triggerPin, GPIO.OUT) 
+        self.buffer = []
+        self.units = {
+            0: ("cm", 100), 
+            1: ("m", 1), 
+            2: ("in", 39.3701), 
+            3: ("ft", 3.28084)
+            }
 
-# Loop: getting distance from HC-SR04
-try:
-    while True:
+        self.unit = unit
+    
+    def shutdownGpio(self):
+        lgpio.gpio_write(self.handle, self.echoPin, 0)
+        lgpio.gpio_write(self.handle, self.triggerPin, 0)
+
+        lgpio.gpiochip_close(self.handle)
+    
+    def getDistance(self):
+
         # Output on high for 10E-6 seconds
-        GPIO.output(outputPin, False)
+        lgpio.gpio_write(self.handle, self.triggerPin, 0)
         time.sleep(2E-6)
-        GPIO.output(outputPin, True)
+        lgpio.gpio_write(self.handle, self.triggerPin, 1)
         time.sleep(10E-6)
-        GPIO.output(outputPin, False)
+        lgpio.gpio_write(self.handle, self.triggerPin, 0)
 
-        # Wait for signal
-        while GPIO.input(inputPin) == 0:
-            pass
+        # Wait for echo to go HIGH
+        start_wait = time.perf_counter()
+        
+        while lgpio.gpio_read(self.handle,self.echoPin) == 0: 
+        # GPIO.input(self.echoPin) == 0:
+            # replace 0.1 with 2 * 4meters *speed of sound
+            if time.perf_counter() - start_wait > 0.03:
+                print("\nNo echo received")
+                return None
 
-        # Mark start of signal
-        start = time.time()
+        start = time.perf_counter()
 
-        # Wait for signal end
-        while GPIO.input(inputPin) == 1:
-            pass
+        # Wait for echo to go LOW
+        while lgpio.gpio_read(self.handle,self.echoPin) == 1: 
+            if time.perf_counter() - start > 0.03:
+                #print("Out of range             ", end = "\r")
+                return None
 
-        # Mark end of signal
-        stop = time.time()
+        stop = time.perf_counter()
 
-        # Calculate and display travel time
         travelTime = stop - start
-        print("Travel Time:", str(int(travelTime * 1E6)))
+        distance_m = (travelTime * self.speedOfSound) / 2
+        distance = distance_m * self.units[self.unit][1] 
 
-        # Buffer
-        time.sleep(0.2)
+        # Add reading to buffer
+        self.buffer.append(distance)
 
-# Use Ctrl + C to end and cleanup
+        # keep last 5 readings
+        if len(self.buffer) > 3:
+            self.buffer.pop(0)
+
+        return statistics.median(self.buffer)
+
+    def getDistanceFiltered(self, samples=5):
+        readings = []
+
+        for _ in range(samples):
+            d = self.getDistance()
+
+            if d is not None:
+                readings.append(d)
+
+            time.sleep(0.005)  # small delay between pings
+
+        if not readings:
+            return None
+
+        return statistics.median(readings)
+
+    def printDistance(self, distance):
+        if (distance <= 400):
+            unitLabel = self.units[self.unit][0]
+            print(f"Travel Distance: {distance:.2f} {unitLabel}          ", end="\r")
+
+
+echoPin = 21
+triggerPin = 20
+
+sensor = HC_SR04(triggerPin, echoPin, 3)
+try:
+    while (True):
+        distance = sensor.getDistanceFiltered()
+        if (distance is not None and distance <= 400):
+            sensor.printDistance(distance)
+
+        time.sleep(0.002)
+
 except KeyboardInterrupt:
-    GPIO.cleanup()
+    sensor.shutdownGpio() 
     print("\nGPIO cleanup successful")
